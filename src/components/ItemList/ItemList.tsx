8b11219bff4f0e36ba4442b5a8560646aba67f35
Heloriel Thrayn <helore.art@gmail.com>
2022-07-12 20:47:55 -0300

    Fix: grid layout items

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -2,43 +2,43 @@ import { Item } from "../Item/Item";
 
 export const ItemList = () => {
   return (
-    <ul className="flex flex-wrap justify-between gap-y-9">
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 min-h-[400px] w-full">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"01"} />
         </a>
       </li>
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"02"} />
         </a>
       </li>
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"03"} />
         </a>
       </li>
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"04"} />
         </a>
       </li>
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"05"} />
         </a>
       </li>
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"06"} />
         </a>
       </li>
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"07"} />
         </a>
       </li>
-      <li className="w-full min-w-[300px] md:max-w-[300px] h-[300px] my-y group">
+      <li className="w-full h-full group">
         <a href="#" className="bg-zinc-300 flex min-w-full min-h-full justify-center items-center">
           <Item id={"08"} />
         </a>
