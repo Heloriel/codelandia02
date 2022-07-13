f3b6a32c96a98da77c9915e8d608234cf7374d0f
Heloriel <andradematheus.dev@gmail.com>
2022-07-13 11:13:30 -0300

    Public path vite

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -3,7 +3,7 @@ import ReactTypingEffect from "react-typing-effect";
 export const Slider = () => {
   return (
     <section className="flex w-full h-[500px]">
-      <div className="w-full max-h-full bg-no-repeat bg-fixed bg-cover bg-center bg-[url(/public/assets/images/sliderbg.jpg)]">
+      <div className="w-full max-h-full bg-no-repeat bg-fixed bg-cover bg-center bg-[url(/assets/images/sliderbg.jpg)]">
         <div className="bg-black/50 flex flex-1 flex-col h-full">
           <div className="flex flex-col flex-1 w-full md:max-w-[50%] h-full justify-center p-6 md:p-24">
             <div className="mb-4">
